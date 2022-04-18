8f516e43d5ef42927346592e28d43ef706272cf9
Suhaib Khan <suhaibklm@gmail.com>
2022-04-18 21:34:08 +0400

    Editor changes

--- src/components/data-editor/ResumeEditor.tsx
+++ src/components/data-editor/ResumeEditor.tsx
@@ -1,4 +1,10 @@
-import React, { MouseEvent, SyntheticEvent, useContext, useState } from 'react';
+import React, {
+  Fragment,
+  MouseEvent,
+  SyntheticEvent,
+  useContext,
+  useState,
+} from 'react';
 import {
   Tab,
   Form,
@@ -8,9 +14,18 @@ import {
   Select,
   Accordion,
   AccordionTitleProps,
+  Icon,
+  Button,
 } from 'semantic-ui-react';
 import { set, get } from '../../utils/utils';
-import { ResumeData } from './ResumeData.model';
+import {
+  Education,
+  Experience,
+  PersonalDetail,
+  ResumeData,
+  Skill,
+  SkillSet,
+} from './ResumeData.model';
 import styles from './ResumeEditor.module.css';
 
 interface FormDef {
@@ -20,7 +35,7 @@ interface FormDef {
   viewOptions?: any;
 }
 
-const MAIN_FORM: FormDef[][] = [
+const SUMMARY_FORM: FormDef[][] = [
   [
     { path: 'docTitle', label: 'Document Title' },
     { path: 'name', label: 'Name' },
@@ -31,13 +46,8 @@ const MAIN_FORM: FormDef[][] = [
     { path: 'email', label: 'Email' },
     { path: 'skype', label: 'Skype' },
   ],
-  [
-    { path: 'github', label: 'Github URL' },
-    { path: 'linkedin', label: 'Linkedin URL' },
-  ],
-];
-
-const SUMMARY_FORM: FormDef[][] = [
+  [{ path: 'github', label: 'Github URL' }],
+  [{ path: 'linkedin', label: 'Linkedin URL' }],
   [
     {
       path: 'summary',
@@ -93,7 +103,12 @@ const EXP_FORM: FormDef[][] = [
 ];
 
 const EXP_DETAILS_FORM: FormDef[] = [
-  { path: 'contributions', label: 'Acheivements', type: 'multiline' },
+  {
+    path: 'contributions',
+    label: 'Acheivements',
+    type: 'multiline',
+    viewOptions: { fluid: false },
+  },
 ];
 
 const EDU_FORM: FormDef[][] = [
@@ -132,30 +147,143 @@ const panes = [
   },
 ];
 
-function SkillsTabPane() {
-  const { data } = useContext(DataContext);
-  const [active, setActive] = useState<AccordionTitleProps['index']>(0);
+interface SecAccordianTitleProps {
+  title: string;
+  index: AccordionTitleProps['index'];
+  active: boolean;
+  onAccClick: (_: MouseEvent, titleProps: AccordionTitleProps) => void;
+  onDelete: (index: AccordionTitleProps['index']) => void;
+}
 
-  const handleAccClick = (_: MouseEvent, { index }: AccordionTitleProps) => {
+function SecAccordianTitle({
+  title,
+  index,
+  active,
+  onAccClick,
+  onDelete,
+}: SecAccordianTitleProps) {
+  const handleDelete = (evt: MouseEvent) => {
+    evt.stopPropagation();
+    onDelete(index);
+  };
+
+  return (
+    <Accordion.Title index={index} active={active} onClick={onAccClick}>
+      <div className={styles.sectionTitle}>
+        <div>
+          <Icon name="dropdown" />
+          {title}
+        </div>
+        <div>
+          <Button
+            negative
+            basic
+            icon="close"
+            circular
+            onClick={handleDelete}
+          ></Button>
+        </div>
+      </div>
+    </Accordion.Title>
+  );
+}
+
+function useAccordionToggle(
+  defaultIndex = 0
+): [
+  AccordionTitleProps['index'],
+  (_: MouseEvent, titleProps: AccordionTitleProps) => void
+] {
+  const [active, setActive] = useState<AccordionTitleProps['index']>(0);
+  const handleAccToggle = (_: MouseEvent, { index }: AccordionTitleProps) => {
     setActive((prevActive) => (prevActive !== index ? index : -1));
   };
+  return [active, handleAccToggle];
+}
+
+function useDataArrayDelete(arrayPath: string) {
+  const { data, onDataChange } = useContext(DataContext);
+
+  const handleDelete = (index: AccordionTitleProps['index']) => {
+    const newArray = get(data, arrayPath).slice(0);
+    newArray.splice(index, 1);
+    onDataChange(set(data, arrayPath, newArray));
+  };
+
+  return handleDelete;
+}
+
+function useDataNestedArrayDelete(arrayPath: string) {
+  const { data, onDataChange } = useContext(DataContext);
+
+  const handleNestedDelete = (parentPrefix: string, index: number) => () => {
+    const nestedArrayPath = `${parentPrefix}.${arrayPath}`;
+    const newArray = get(data, nestedArrayPath).slice(0);
+    newArray.splice(index, 1);
+    onDataChange(set(data, nestedArrayPath, newArray));
+  };
+
+  return handleNestedDelete;
+}
+
+function useDataArrayAdd<T>(arrayPath: string, emptyVal: T) {
+  const { data, onDataChange } = useContext(DataContext);
+  const handleAdd = () => {
+    const arrayVal = get(data, arrayPath);
+    if (!Array.isArray(arrayVal)) {
+      console.log('Invalid operation on non array field');
+      return;
+    }
+    onDataChange(set(data, `${arrayPath}[${arrayVal.length}]`, emptyVal));
+  };
+
+  return handleAdd;
+}
+
+function useDataNestedArrayAdd<T>(arrayPath: string, emptyVal: T) {
+  const { data, onDataChange } = useContext(DataContext);
+  const handleNestedAdd = (parentPrefix: string) => () => {
+    const nestedArrayPath = `${parentPrefix}.${arrayPath}`;
+    const arrayVal = get(data, nestedArrayPath);
+    if (!Array.isArray(arrayVal)) {
+      console.log('Invalid operation on non array field');
+      return;
+    }
+    onDataChange(set(data, `${nestedArrayPath}[${arrayVal.length}]`, emptyVal));
+  };
+
+  return handleNestedAdd;
+}
+
+function SkillsTabPane() {
+  const { data } = useContext(DataContext);
+  const [active, onAccToggle] = useAccordionToggle(0);
+  const onDelete = useDataArrayDelete('skillSet');
+  const onAdd = useDataArrayAdd<SkillSet>('skillSet', {
+    category: '',
+    skills: [{ label: '', rating: 1 }],
+  });
+  const onSkillAdd = useDataNestedArrayAdd<Skill>('skills', {
+    label: '',
+    rating: 1,
+  });
+  const onSkillDelete = useDataNestedArrayDelete('skills');
 
   return (
     <Tab.Pane>
       <Form>
         <Accordion fluid styled>
           {data?.skillSet?.map((skillSet, index) => (
-            <>
-              <Accordion.Title
+            <Fragment key={`skillSet[${index}]`}>
+              <SecAccordianTitle
+                title={`Skills Category ${index + 1}`}
                 index={index}
                 active={index === active}
-                onClick={handleAccClick}
-              >
-                {`Skills Category ${index + 1}`}
-              </Accordion.Title>
+                onAccClick={onAccToggle}
+                onDelete={onDelete}
+              />
               <Accordion.Content active={index === active}>
                 <FormRenderer
-                  key={`skillSet[${index}]`}
                   formDef={SKILLSET_FORM}
                   pathPrefix={`skillSet[${index}]`}
                 />
@@ -165,14 +293,36 @@ function SkillsTabPane() {
                       <FormRenderer
                         formDef={SKILLS_FORM}
                         pathPrefix={`skillSet[${index}].skills[${sIndex}]`}
+                        onItemDelete={onSkillDelete(
+                          `skillSet[${index}]`,
+                          sIndex
+                        )}
                       />
                     </Form.Group>
                   ))}
+                  <div className={styles.nestAddBtnContainer}>
+                    <Button
+                      positive
+                      icon="add"
+                      content="Add Skill"
+                      labelPosition="left"
+                      onClick={onSkillAdd(`skillSet[${index}]`)}
+                    />
+                  </div>
                 </div>
               </Accordion.Content>
-            </>
+            </Fragment>
           ))}
         </Accordion>
+        <div className={styles.addBtnContainer}>
+          <Button
+            positive
+            icon="add"
+            content="Add Skills Category"
+            labelPosition="left"
+            onClick={onAdd}
+          />
+        </div>
       </Form>
     </Tab.Pane>
   );
@@ -180,28 +330,33 @@ function SkillsTabPane() {
 
 function ExpTabPane() {
   const { data } = useContext(DataContext);
-  const [active, setActive] = useState<AccordionTitleProps['index']>(0);
-
-  const handleAccClick = (_: MouseEvent, { index }: AccordionTitleProps) => {
-    setActive((prevActive) => (prevActive !== index ? index : -1));
-  };
+  const [active, onAccToggle] = useAccordionToggle(0);
+  const onDelete = useDataArrayDelete('experience');
+  const onAdd = useDataArrayAdd<Experience>('experience', {
+    company: '',
+    role: '',
+    dateRange: '',
+    location: '',
+    contributions: [''],
+  });
+  const onContribAdd = useDataNestedArrayAdd<string>('contributions', '');
+  const onContribDelete = useDataNestedArrayDelete('contributions');
 
   return (
     <Tab.Pane>
       <Form>
         <Accordion fluid styled>
           {data?.experience?.map((exp, index) => (
-            <>
-              <Accordion.Title
+            <Fragment key={`experience[${index}]`}>
+              <SecAccordianTitle
+                title={`Experience ${index + 1}`}
                 index={index}
                 active={index === active}
-                onClick={handleAccClick}
-              >
-                {`Experience ${index + 1}`}
-              </Accordion.Title>
+                onAccClick={onAccToggle}
+                onDelete={onDelete}
+              />
               <Accordion.Content active={index === active}>
                 <FormRenderer
-                  key={`experience[${index}]`}
                   formDef={EXP_FORM}
                   pathPrefix={`experience[${index}]`}
                 />
@@ -211,12 +366,34 @@ function ExpTabPane() {
                     formDef={EXP_DETAILS_FORM}
                     pathPrefix={`experience[${index}]`}
                     pathIndex={cIndex}
+                    onItemDelete={onContribDelete(
+                      `experience[${index}]`,
+                      cIndex
+                    )}
                   />
                 ))}
+                <div className={styles.nestAddBtnContainer}>
+                  <Button
+                    positive
+                    icon="add"
+                    content="Add Achievements"
+                    labelPosition="left"
+                    onClick={onContribAdd(`experience[${index}]`)}
+                  />
+                </div>
               </Accordion.Content>
-            </>
+            </Fragment>
           ))}
         </Accordion>
+        <div className={styles.addBtnContainer}>
+          <Button
+            positive
+            icon="add"
+            content="Add Experience"
+            labelPosition="left"
+            onClick={onAdd}
+          />
+        </div>
       </Form>
     </Tab.Pane>
   );
@@ -224,35 +401,46 @@ function ExpTabPane() {
 
 function EducationTabPane() {
   const { data } = useContext(DataContext);
-  const [active, setActive] = useState<AccordionTitleProps['index']>(0);
-
-  const handleAccClick = (_: MouseEvent, { index }: AccordionTitleProps) => {
-    setActive((prevActive) => (prevActive !== index ? index : -1));
-  };
+  const [active, onAccToggle] = useAccordionToggle(0);
+  const onDelete = useDataArrayDelete('education');
+  const onAdd = useDataArrayAdd<Education>('education', {
+    school: '',
+    degree: '',
+    dateRange: '',
+    details: '',
+  });
 
   return (
     <Tab.Pane>
       <Form>
         <Accordion fluid styled>
           {data?.education?.map((edu, index) => (
-            <>
-              <Accordion.Title
+            <Fragment key={`education[${index}]`}>
+              <SecAccordianTitle
+                title={`Education ${index + 1}`}
                 index={index}
                 active={index === active}
-                onClick={handleAccClick}
-              >
-                {`Education ${index + 1}`}
-              </Accordion.Title>
+                onAccClick={onAccToggle}
+                onDelete={onDelete}
+              />
               <Accordion.Content active={index === active}>
                 <FormRenderer
-                  key={`education[${index}]`}
                   formDef={EDU_FORM}
                   pathPrefix={`education[${index}]`}
                 />
               </Accordion.Content>
-            </>
+            </Fragment>
           ))}
         </Accordion>
+        <div className={styles.addBtnContainer}>
+          <Button
+            positive
+            icon="add"
+            content="Add Education"
+            labelPosition="left"
+            onClick={onAdd}
+          />
+        </div>
       </Form>
     </Tab.Pane>
   );
@@ -260,6 +448,10 @@ function EducationTabPane() {
 
 function PersDetailsTabPane() {
   const { data } = useContext(DataContext);
+  const onAdd = useDataArrayAdd<PersonalDetail>('personalDetails', {
+    label: '',
+    value: '',
+  });
 
   return (
     <Tab.Pane>
@@ -271,6 +463,15 @@ function PersDetailsTabPane() {
             pathPrefix={`personalDetails[${index}]`}
           />
         ))}
+        <div className={styles.nestAddBtnContainer}>
+          <Button
+            positive
+            icon="add"
+            content="Add Personal Details"
+            labelPosition="left"
+            onClick={onAdd}
+          />
+        </div>
       </Form>
     </Tab.Pane>
   );
@@ -295,12 +496,14 @@ interface FormRendererProps {
   formDef: FormDef[][] | FormDef[];
   pathPrefix?: string;
   pathIndex?: number;
+  onItemDelete?: () => void;
 }
 
 const FormRenderer = ({
   formDef,
   pathPrefix = '',
   pathIndex,
+  onItemDelete,
 }: FormRendererProps) => {
   const { data, onDataChange } = useContext(DataContext);
 
@@ -319,12 +522,13 @@ const FormRenderer = ({
     return fullPath;
   };
 
+  const multiRow = Array.isArray(formDef[0]);
+
   return (
     <>
-      {formDef.map((formGrp: FormDef[] | FormDef) => (
-        <>
-          {Array.isArray(formGrp) ? (
-            <Form.Group widths="equal">
+      {multiRow
+        ? (formDef as FormDef[][]).map((formGrp, rowIndex) => (
+            <Form.Group widths="equal" key={`row-${rowIndex}`}>
               {formGrp.map(({ label, path, type, viewOptions }) => (
                 <Form.Field
                   key={getFullPath(path)}
@@ -338,20 +542,30 @@ const FormRenderer = ({
                 />
               ))}
             </Form.Group>
-          ) : (
+          ))
+        : (formDef as FormDef[]).map((form) => (
             <Form.Field
-              key={getFullPath(formGrp.path)}
+              key={getFullPath(form.path)}
               fluid
-              control={typeElemMap[formGrp.type || 'input']}
-              label={formGrp.label}
-              placeholder={formGrp.label}
-              onChange={handleChange(getFullPath(formGrp.path))}
-              value={get(data, getFullPath(formGrp.path))}
-              {...formGrp.viewOptions}
+              control={typeElemMap[form.type || 'input']}
+              label={form.label}
+              placeholder={form.label}
+              onChange={handleChange(getFullPath(form.path))}
+              value={get(data, getFullPath(form.path))}
+              {...form.viewOptions}
             />
-          )}
-        </>
-      ))}
+          ))}
+      {onItemDelete && (
+        <div className={styles.deleteBtn}>
+          <Button
+            negative
+            basic
+            icon="close"
+            onClick={onItemDelete}
+            style={{ boxShadow: 'none !important' }}
+          ></Button>
+        </div>
+      )}
     </>
   );
 };
@@ -361,9 +575,6 @@ function ResumeEditor({ data, onDataChange }: ResumeEditorProps) {
     <div>
       <h3>Resume Editor</h3>
       <DataContext.Provider value={{ data, onDataChange }}>
-        <Form>
-          <FormRenderer formDef={MAIN_FORM} />
-        </Form>
         <Tab
           menu={{
             color: 'blue',
